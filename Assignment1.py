f94c86bcd5f539bf909d6f4d9636e95d4b3d516f
ahmadzee786 <68564273+ahmadzee786@users.noreply.github.com>
2025-08-06 16:28:12 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+# write a python program that does the following:
+
+# take.1 = Takes two numbers as input from the user.
+
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
+
+addtion = num1 + num2
+subs = num1 - num2
+multipy = num1 * num2
+devide = num1 / num2
+
+'''task.2 = Performs the basic mathematical operations on these two numbers.
+addition
+substraction
+multiplication
+devision
+'''
+
+print("\naddition  is : ",addtion)
+print("subtraction  is : ",subs)
+print("multipy  is : ",multipy)
+print("devide  is : ",devide)
---
+++ Assignment2.py
@@ -0,0 +1,5 @@
+# task2 = create a Personalized Greeting
+
+fname = input("Enter the First name : ")
+lname = input("Enter the last name : ")
+print(f"Hello {fname + " " + lname}! Welcome to the python program.")
